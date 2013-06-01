c9fcee29bac332ba29099a82689cf1c7b26b8279
ovasylev <ovasylev@me.com>
2013-06-01 18:54:46 -0400

    Added 2 files: git_hello.py and git_hello.png

--- swampy-package/swampy/sib_hello.py
+++ swampy-package/swampy/sib_hello.py
@@ -1,6 +1,6 @@
 # Hello excercise from Week 0
 
-# Name:
+# Name: Olha Vasylevska
 
 
 from TurtleWorld import * 		
@@ -12,9 +12,77 @@ bob = Turtle()
 # This is where you put code to move bob
 
 
+def h(t, dist):
+	rt(t)
+	fd(t, dist)
+	pu(t)
+	bk(t, dist/2)
+	lt(t)
+	pd(t)
+	fd(t, dist*0.4)
+	lt(t)
+	pu(t)
+	fd(t, dist/2)
+	pd(t)
+	bk(t, dist)
+	pu(t)
+	fd(t, dist)
+	rt(t)
+	fd(t, dist*0.15)
+	pd(t)
 
+def e(t, dist):
+	rt(t)
+	fd(t, dist)
+	lt(t)
+	fd(t, dist*0.4)
+	pu(t)
+	lt(t)
+	fd(t, dist/2)
+	pd(t)
+	lt(t)
+	fd(t, dist*0.4)
+	pu(t)
+	rt(t)
+	fd(t, dist/2)
+	pd(t)
+	rt(t)
+	fd(t, dist*0.4)
+	pu(t)
+	fd(t, dist*0.15)
+	pd(t)
 
+def l(t, dist):
+	rt(t)
+	fd(t, dist)
+	lt(t)
+	fd(t, dist*0.4)
+	lt(t)
+	pu(t)
+	fd(t, dist)
+	rt(t)
+	fd(t, dist*0.15)
+	pd(t)
 
+def o(t, dist):
+	rt(t)
+	fd(t, dist)
+	lt(t)
+	fd(t, dist*0.4)
+	lt(t)
+	fd(t, dist)
+	rt(t)
+	bk(t, dist*0.4)
+	pu(t)
+	fd(t, dist*0.4)
+	fd(t, dist*0.15)
+	pd(t)
+
+h(bob, 50)
+e(bob, 50)
+l(bob, 50)
+l(bob, 50)
+o(bob, 50)
 
 
 
