48f3ed7671f84adb83569053dbee83ec3716aaa5
ovasylev <ovasylev@me.com>
2013-06-04 15:21:33 -0400

    Flowers and answers are ready.

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,22 +1,52 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name:Olha Vasylevska
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
 
-
-
 # This is where you put code to move bob
 
+import math
 
 
+def arc(t, r, theta):
+	s=(float(theta)/180)*math.pi*r
+	n=int(s)+1
+	seg_angle=float(theta)/n
+
+	for i in range(n):
+		fd(t, 1)
+		lt(t, seg_angle)
 
 
+def petal(t, r, angle):
+	for i in range(2):
+		arc(t, r, angle)
+		lt(t, 180-angle)
 
+def flower(t, r, n, angle):
+	for i in range(n):
+		petal(t, r, angle)
+		lt(t, 360.0/n)
+
+bob.delay=0.01
+
+flower(bob, 65, 7, 55)
+
+pu(bob)
+fd(bob, 150)
+pd(bob)
+
+flower(bob, 40, 10, 75)
+
+pu(bob)
+fd(bob, 150)
+pd(bob)
+
+flower(bob, 120, 20, 20)
 
 
 wait_for_user()					
-
